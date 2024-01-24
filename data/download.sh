6a40e0fd5fb82c7cc206c3705d5d4bf310fcdea0
fangzhouli <fzli0805@gmail.com>
2024-01-23 19:22:00 -0800

    uploaded data

---
+++ data/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# # Download the data from USDA FDC.
+# https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_foundation_food_csv_2023-10-26.zip
+# https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_sr_legacy_food_csv_2018-04.zip
+
+# # Download the data from FoodAtlas zip.
+
+# # Download the data from FooDB.
+# https://foodb.ca/public/system/downloads/foodb_2020_04_07_json.zip
