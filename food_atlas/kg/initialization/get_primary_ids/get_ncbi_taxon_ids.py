b89bcdbfbc4db64c5b37587fdd41a237917a92e3
fangzhouli <fzli0805@gmail.com>
2024-02-04 18:03:32 -0800

    refactored

---
+++ food_atlas/kg/initialization/get_primary_ids/get_ncbi_taxon_ids.py
@@ -0,0 +1,88 @@
+import pandas as pd
+from Bio import Entrez
+
+with open("food_atlas/kg/initialization/get_ids/api_key.txt") as f:
+    Entrez.email = f.readline().strip()
+    Entrez.api_key = f.readline().strip()
+
+
+def get_ncbi_taxon_ids_from_fdc():
+    PATH_DATA_DIR = "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26"
+
+    data = pd.read_csv(f"{PATH_DATA_DIR}/food.csv")
+    ids_ff = pd.read_csv(f"{PATH_DATA_DIR}/foundation_food.csv")['fdc_id']
+    data = data[data['fdc_id'].isin(ids_ff)]
+    attrs_ff = pd.read_csv(f"{PATH_DATA_DIR}/food_attribute.csv").set_index('fdc_id')
+
+    def extract_external_ids(row):
+        result = {
+            'ncbi_taxon_url': None,
+        }
+        if row['fdc_id'] in attrs_ff.index:
+            attr = attrs_ff.loc[row['fdc_id']]
+            if type(attr) == pd.Series:
+                attr = pd.DataFrame(attr).T
+            attr = attr.set_index('name')
+
+            for attr_name, col_name in zip(attr_names, col_names):
+                result[col_name] = attr.loc[attr_name, 'value'] \
+                    if attr_name in attr.index else None
+
+        return pd.Series(result)
+
+    attr_names = ['NCBI Taxon']
+    col_names = ['ncbi_taxon_url']
+    data = pd.concat(
+        [data, data.apply(extract_external_ids, axis=1)],
+        axis=1
+    )
+
+    # Parse NCBI Taxon ID from URL.
+    def parse_ncbi_taxon_url(url):
+        if url is None:
+            return None
+        else:
+            id_ = url.split('/')[-1]
+            if id_.isdigit():
+                return int(id_)
+            elif id_.startswith('NCBITaxon_'):
+                return int(id_.split('_')[-1])
+            else:
+                return int(id_.split('=')[-1])
+
+    data['ncbi_taxon_id'] = data['ncbi_taxon_url'].apply(parse_ncbi_taxon_url)
+
+    return data['ncbi_taxon_id'].dropna().astype(int).tolist()
+
+
+def get_ncbi_taxon_ids_from_foodb():
+    data = pd.read_json("data/FooDB/Food.json", lines=True)
+    ncbi_taxon_ids = data['ncbi_taxonomy_id'].dropna().astype(int).tolist()
+
+    # Also extract from scientific names.
+    scientific_names = data.query(
+        "ncbi_taxonomy_id.isna() & name_scientific.notna() & name_scientific != ''"
+    )['name_scientific'].str.lower().tolist()
+
+    for scientific_name in scientific_names:
+        # Reformat.
+        scientific_name \
+            = scientific_name.replace('× ', 'x ').replace(' ssp. ', ' subsp. ')
+        # Remove authority.
+        scientific_name \
+            = scientific_name.replace(' l.', '').replace(' f. alba dc.', '')
+        handle = Entrez.esearch(db='taxonomy', term=scientific_name)
+        record = Entrez.read(handle)
+        if len(record['IdList']) == 1:
+            ncbi_taxon_ids.append(int(record['IdList'][0]))
+
+    return ncbi_taxon_ids
+
+
+if __name__ == '__main__':
+    ncbi_taxon_ids_fdc = get_ncbi_taxon_ids_from_fdc()
+    ncbi_taxon_ids_foodb = get_ncbi_taxon_ids_from_foodb()
+
+    with open('outputs/kg/initialization/ncbi_taxon_ids.txt', 'w') as f:
+        for id_ in list(set(ncbi_taxon_ids_fdc + ncbi_taxon_ids_foodb)):
+            f.write(f"{id_}\n")
---
+++ food_atlas/kg/initialization/get_primary_ids/get_pubchem_cids.py
@@ -0,0 +1,118 @@
+import pandas as pd
+
+
+def get_pubchem_cid_from_fdc():
+    chemicals = pd.read_csv(
+        "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/nutrient.csv"
+    )
+    chemical_ids_used = pd.read_csv(
+        "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/food_nutrient.csv",
+        usecols=['nutrient_id'],
+    )['nutrient_id'].unique().tolist()
+    chemicals = chemicals.query(f"id in {chemical_ids_used}").set_index('id')
+
+    attrs = pd.read_excel(
+        "data/FDC/Phytochemical_Spreadsheet.xlsx",
+        sheet_name='FDC-Compounds(nutr)',
+    ).set_index('id')
+
+    def _get_pubchem_cid(row):
+        if row.name in attrs.index:
+            pubchem_cids = attrs.loc[[row.name], 'pubchem_compound_id'].tolist()
+            if len(pubchem_cids) == 1:
+                return pubchem_cids[0]
+            elif len(pubchem_cids) > 1:
+                return pubchem_cids[0]
+
+        return None
+
+    chemicals['pubchem_cid_from_kay'] \
+        = chemicals.apply(_get_pubchem_cid, axis=1).astype('Int64')
+    chemicals['name'].to_csv(
+        "outputs/kg/initialization/chemical_names_for_PIES_fdc_1.txt",
+        index=False,
+        header=False,
+    )
+    chemicals['name_formatted'] = ''
+    chemicals = chemicals[[
+        'name', 'name_formatted', 'pubchem_cid_from_kay',
+    ]]
+
+    # Use the PubChem Identifier Exchange Service to get the PubChem CID from the name.
+    pubchem_cids = pd.read_csv(
+        "outputs/kg/initialization/PIES_fdc_1.txt",
+        header=None,
+        sep='\t',
+        names=['name', 'pubchem_cid'],
+        dtype={'name': str, 'pubchem_cid': 'Int64'}
+    ).set_index('name')
+
+    def _get_pubchem_cid_from_query(row):
+        if row['name'] in pubchem_cids.index:
+            cids = pubchem_cids.loc[
+                [row['name']], 'pubchem_cid'
+            ].dropna().unique().tolist()
+            if len(cids) == 1:
+                return cids[0]
+            elif len(cids) > 1:
+                return cids
+        return None
+
+    chemicals['pubchem_cid_from_query'] = chemicals.apply(
+        _get_pubchem_cid_from_query, axis=1
+    )
+
+    def _get_name_formatted(row):
+        if row['pubchem_cid_from_query'] is not None:
+            return 'SKIP'
+
+        terms = row['name'].strip().split(', ')
+        if len(terms) == 2:
+            if len(terms[1]) <= 2 and ' ' not in terms[0]:
+                # Element.
+                return terms[0]
+            elif terms[1] in ['alpha', 'beta', 'gamma', 'delta']:
+                # Different forms of the compound with unique CID.
+                return f"{terms[1]}-{terms[0]}"
+            else:
+                return "SKIP_MANUAL"
+        else:
+            if 'FA ' in row['name']:
+                return "SKIP_LIPIDS"
+            else:
+                return "SKIP_MANUAL"
+
+    chemicals['name_formatted'] = chemicals.apply(
+        _get_name_formatted, axis=1
+    )
+    with open("outputs/kg/initialization/chemical_names_for_PIES_fdc_2.txt", 'w') as f:
+        f.write(
+            '\n'.join(
+                [
+                    x for x in chemicals['name_formatted'].dropna().tolist()
+                    if 'SKIP' not in x
+                ]
+            )
+        )
+
+    # Use the PubChem Identifier Exchange Service to get the PubChem CID from the name.
+    pubchem_cids_for_not_found = pd.read_csv(
+        "outputs/kg/initialization/PIES_fdc_2.txt",
+        header=None,
+        sep='\t',
+        names=['name', 'pubchem_cid'],
+        dtype={'name': str, 'pubchem_cid': 'Int64'}
+    ).set_index('name')
+
+    chemicals = chemicals.reset_index().set_index('name_formatted')
+    chemicals.loc[pubchem_cids_for_not_found.index, 'pubchem_cid_from_query'] \
+        = pubchem_cids_for_not_found['pubchem_cid']
+    chemicals = chemicals.reset_index().set_index('id')
+    chemicals.to_csv('outputs/kg/initialization/pubchem_cids_fdc.tsv', sep='\t')
+
+    # Manually get PubChem CIDs for SKIP_MANUAL, and save the file as
+    # `pubchem_cids_fdc_manual.tsv`.
+
+
+if __name__ == '__main__':
+    get_pubchem_cid_from_fdc()
