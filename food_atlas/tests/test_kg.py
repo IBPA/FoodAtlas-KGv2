77260d28cc4fd92d3f5b4dc1f477963b48ee740f
fangzhouli <fzli0805@gmail.com>
2024-02-15 15:05:33 -0800

    added unit test and playground

--- food_atlas/tests/test_kg.py
+++ food_atlas/tests/test_kg.py
@@ -1,85 +1,39 @@
-import csv
-
-import numpy as np
-import pandas as pd
-
 from ..kg import KnowledgeGraph
-
-# # Check all sysnonyms given one entity can be linked back to the entity in LUT.
-# pass
 
 # # Check all LUT entities are in the entities.
 # pass
 
 # # Check triplets from Lit2KG
 
-def load_pilot_triplets():
-    triplets = pd.read_excel(
-        "food_atlas/tests/evaluation_v1_gpt3.5_gpt4.xlsx",
-        sheet_name="Sheet1 (2)",
-    )
-    triplets = triplets.dropna(subset=['GPT4'])
-    triplets['reference'] = triplets['sentence'].ffill()
-
-    def _tripletify(s):
-        nonlocal errors
-
-        if pd.isna(s):
-            return ['', '', '', '']
-
-        s = next(csv.reader([s], delimiter=',', quotechar='"', skipinitialspace=True))
-        if len(s) != 4:
-            errors += [s]
-            return ['', '', '', '']
-
-        return s
-
-    errors = []
-    triplets['triplets'] = triplets['GPT4'].apply(_tripletify)
-    triplets[['head', 'food_part', 'tail', 'conc']] = pd.DataFrame(
-        triplets['triplets'].tolist(), index=triplets.index
-    ).apply(lambda col: col.str.strip().str.lower())
-    triplets = triplets.query("head != '' and tail != ''").copy()
-    triplets['_extracted_conc'] = triplets['conc']
-    triplets['_extracted_food_part'] = triplets['food_part']
-    triplets['conc_value'] = None
-    triplets['conc_unit'] = None
-    triplets['food_part'] = None
-    triplets['food_processing'] = None
-    triplets['source'] = 'lit2kg:gpt-4'
-    triplets['quality_score'] = None
-    triplets['relationship'] = 'contains'
-    triplets = triplets[[
-        'head', 'relationship', 'tail', 'conc_value', 'conc_unit', 'food_part',
-        'food_processing', 'source', 'reference', 'quality_score', '_extracted_conc',
-        '_extracted_food_part',
-    ]]
-
-    return triplets
-
 
 if __name__ == '__main__':
-    triplets = load_pilot_triplets()
     kg = KnowledgeGraph()
-    kg.add_triplets(triplets)
 
+    # Test 1. All synonyms in the entities can be found in LUT.
+    def _check_entity_name_in_lut(row):
+        for name in row['synonyms']:
+            if row['entity_type'] == 'food':
+                assert name in kg._lut_food, "Test 1 failed."
+            elif row['entity_type'] == 'chemical':
+                assert name in kg._lut_chemical, "Test 1 failed."
+            else:
+                raise NotImplementedError()
 
-    # lut_food, lut_chem = load_lookup_tables()
+    kg._entities.apply(_check_entity_name_in_lut, axis=1)
 
-    # foods = triplets.loc[triplets['food'] != '', 'food'].unique()
-    # n_not_in_lut = 0
-    # for food in foods:
-    #     if food not in lut_food:
-    #         # print(food)
-    #         n_not_in_lut += 1
-    # print(f"{n_not_in_lut}/{len(foods)} unique foods not in LUT")
+    # Test 2. All entities in the lookup table are in the entities.
+    entities_in_lut = set()
+    for entities in kg._lut_food.values():
+        entities_in_lut.update(entities)
+    for entities in kg._lut_chemical.values():
+        entities_in_lut.update(entities)
+    entities = set(kg._entities['foodatlas_id'])
+    assert len(entities) == len(kg._entities)
+    assert entities_in_lut - entities == set(), "Test 2 failed."
 
-    # chems = triplets.loc[triplets['chem'] != '', 'chem'].unique()
-    # n_not_in_lut = 0
-    # for chem in chems:
-    #     if chem not in lut_chem:
-    #         n_not_in_lut += 1
-    #     else:
-    #         print(chem)
+    # Test 3. All triplets have valid entities.
+    assert kg._triplets['foodatlas_id'].nunique() == len(kg._triplets)
 
-    # print(f"{n_not_in_lut}/{len(chems)} unique chems not in LUT")
+    # Test 4. Use food to retrieve existing triplets and metadata.
+    food = "apple"
+    kg.get_triplets_by_food_name(food)
