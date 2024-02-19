c75172278ece6d9a42056b33dc0f8a2df3514e5c
fangzhouli <fzli0805@gmail.com>
2024-02-19 15:34:14 -0800

    minor issue

--- food_atlas/kg/initialization/run_initialization.sh
+++ food_atlas/kg/initialization/run_initialization.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-python -m food_atlas.kg.initialization.get_primary_ids.get_ncbi_taxon_ids
-python -m food_atlas.kg.initialization.get_primary_ids.get_pubchem_cids
+# python -m food_atlas.kg.initialization.get_primary_ids.get_ncbi_taxon_ids
+# python -m food_atlas.kg.initialization.get_primary_ids.get_pubchem_cids
 
-python -m food_atlas.kg.initialization.retrieve_dbs.retrieve_ncbi_taxonomy
-python -m food_atlas.kg.initialization.retrieve_dbs.retrieve_pubchem_compound
+# python -m food_atlas.kg.initialization.retrieve_dbs.retrieve_ncbi_taxonomy
+# python -m food_atlas.kg.initialization.retrieve_dbs.retrieve_pubchem_compound
 
 python -m food_atlas.kg.initialization.init_kg.init_food_entities
 python -m food_atlas.kg.initialization.init_kg.init_chemical_entities
+python -m food_atlas.kg.initialization.init_kg.append_lut_with_ids
