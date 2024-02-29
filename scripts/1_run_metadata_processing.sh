e4ef7dab6b982c35b265157a1ed3501a30df0bb6
fangzhouli <fzli0805@gmail.com>
2024-02-29 15:46:01 -0800

    initial refactoring tested with fd

---
+++ scripts/1_run_metadata_processing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# CURR_TIME=$(date "+%Y%m%d%H%M%S")
+# mkdir -p outputs/kg/$CURR_TIME
+# echo "Created directory: outputs/kg/$CURR_TIME"
+
+# PATH_WORKSPACE=outputs/kg/$CURR_TIME
+# PATH_WORKSPACE=outputs/kg/20240222190741
+
+PATH_INPUT=data/Lit2KG/text_parser_predicted_2024_02_25.pkl
+PATH_OUTPUT_DIR=outputs/kg/20240315
+
+python -m food_atlas.kg.run_metadata_processing \
+    $PATH_INPUT \
+    $PATH_OUTPUT_DIR
--- scripts/2_run_adding_triplets_from_metadata.sh
+++ scripts/2_run_adding_triplets_from_metadata.sh
@@ -4,10 +4,11 @@
 # mkdir -p outputs/kg/$CURR_TIME
 # echo "Created directory: outputs/kg/$CURR_TIME"
 
-PATH_DATA_LIT2KG=data/Lit2KG/tests/text_parser_predicted.pkl
 # PATH_WORKSPACE=outputs/kg/$CURR_TIME
-PATH_WORKSPACE=outputs/kg/20240214102617
+# PATH_WORKSPACE=outputs/kg/20240222190741
+PATH_WORKSPACE=outputs/kg/merge_dbs/fa_v1
 
+# PATH_DATA_LIT2KG=data/Lit2KG/tests/text_parser_predicted.pkl
 # python -m food_atlas.kg.run_data_preparation \
 #     $PATH_DATA_LIT2KG \
 #     $PATH_WORKSPACE
