90facb9fd68e7cb7186d4f8037d4bc53ddc88da1
fangzhouli <fzli0805@gmail.com>
2024-02-29 11:09:24 -0800

    minor fix

--- food_atlas/kg/merge_dbs/run_metadata_processing_fdc.py
+++ food_atlas/kg/merge_dbs/run_metadata_processing_fdc.py
@@ -1,26 +1,27 @@
-from itertools import product
-
 import pandas as pd
 
 from ..utils import load_lookup_tables, constants
 
 
-def get_triplets():
+if __name__ == '__main__':
+    fdc_ids_ff = pd.read_csv(
+        "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/foundation_food.csv"
+    )['fdc_id']
+
     food = pd.read_csv(
         "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/food.csv",
         usecols=['fdc_id', 'description'],
     ).set_index('fdc_id')
+
     nutrient = pd.read_csv(
         "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/nutrient.csv",
         usecols=['id', 'name', 'unit_name'],
     ).set_index('id')
+
     food_nutrient = pd.read_csv(
         "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/food_nutrient.csv",
         usecols=['id', 'fdc_id', 'nutrient_id', 'amount'],
     )
-    fdc_ids_ff = pd.read_csv(
-        "data/FDC/FoodData_Central_foundation_food_csv_2023-10-26/foundation_food.csv"
-    )['fdc_id']
     food_nutrient = food_nutrient[food_nutrient['fdc_id'].isin(fdc_ids_ff)]
     food_nutrient = food_nutrient.query("nutrient_id != 2066").copy()
     food_nutrient['food_name'] = food.loc[food_nutrient['fdc_id'], 'description'].values
@@ -29,28 +30,28 @@ def get_triplets():
         = nutrient.loc[food_nutrient['nutrient_id'], 'name'].values
     food_nutrient['chem_name'] = food_nutrient['chem_name'].str.strip().str.lower()
 
-    lut_food, lut_chem = load_lookup_tables()
+    lut_food, lut_chem = load_lookup_tables("outputs/kg/initialization")
 
-    def _get_triplet(row):
-        nonlocal triplets, containing, foodatlas_tid_curr, foodatlas_mid_curr
+    def _get_metadatum(row):
+        global metadata_rows
 
         fdc_id = row['fdc_id']
         fdc_nutrient_id = row['nutrient_id']
         key_food = constants.get_lookup_key_by_id("fdc_ids", fdc_id)
         key_chem = constants.get_lookup_key_by_id("fdc_nutrient_ids", fdc_nutrient_id)
+        if key_food not in lut_food:
+            print(f"Food not found: {key_food}")
+            return
         if key_chem not in lut_chem:
             print(f"Chemical not found: {key_chem}")
             return
 
         conc_value = row['amount']
         conc_unit = f"{nutrient.loc[fdc_nutrient_id, 'unit_name'].lower()}/100g"
-        containing_ = {
-            'foodatlas_id': f"mc{foodatlas_mid_curr}",
-            'tids': [],
-            'food_name': food.loc[fdc_id, 'description'].strip().lower(),
-            'chemical_name': nutrient.loc[fdc_nutrient_id, 'name'].strip().lower(),
-            'conc_value': conc_value,
-            'conc_unit': conc_unit,
+
+        metadata_row = {
+            'conc_value': None,
+            'conc_unit': None,
             'food_part': None,
             'food_processing': None,
             'source': 'fdc',
@@ -59,34 +60,20 @@ def get_triplets():
                 f"{fdc_id}/nutrients",
             },
             'quality_score': None,
-            '_extracted_conc': '',
-            '_extracted_food_part': '',
+            '_food_name': key_food,
+            '_chemical_name': key_chem,
+            '_conc': f'{conc_value} {conc_unit}',
+            '_food_part': '',
         }
-        for head_id, tail_id in product(lut_food[key_food], lut_chem[key_chem]):
-            if (head_id, tail_id) not in triplets_ht:
-                triplets_ht[(head_id, tail_id)] = f"t{foodatlas_tid_curr}"
-                triplets += [{
-                    'foodatlas_id': triplets_ht[(head_id, tail_id)],
-                    'head_id': head_id,
-                    'relationship_id': 'r1',
-                    'tail_id': tail_id,
-                }]
-                foodatlas_tid_curr += 1
-            containing_['tids'] += [triplets_ht[(head_id, tail_id)]]
-        containing += [containing_]
-        foodatlas_mid_curr += 1
 
-    foodatlas_tid_curr = 1
-    foodatlas_mid_curr = 1
-    triplets_ht = {}
-    triplets = []
-    containing = []
-    food_nutrient.apply(_get_triplet, axis=1)
-    triplets = pd.DataFrame(triplets)
-    containing = pd.DataFrame(containing)
+        metadata_rows += [metadata_row]
 
-    triplets.to_csv('outputs/kg/triplets.tsv', sep='\t', index=False)
-    containing.to_csv('outputs/kg/mdata_contains.tsv', sep='\t', index=False)
+    metadata_rows = []
+    food_nutrient.apply(_get_metadatum, axis=1)
+    metadata = pd.DataFrame(metadata_rows)
 
-if __name__ == '__main__':
-    get_triplets()
+    metadata.to_csv(
+        "outputs/kg/merge_dbs/fdc/_metadata_new.tsv",
+        sep='\t',
+        index=False,
+    )
