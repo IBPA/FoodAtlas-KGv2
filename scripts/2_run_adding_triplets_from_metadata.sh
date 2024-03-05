cae5beb567e9c6992476789cf0e71c01ec21458a
fangzhouli <fzli0805@gmail.com>
2024-03-04 19:52:55 -0800

    added refactored scripts

--- scripts/2_run_adding_triplets_from_metadata.sh
+++ scripts/2_run_adding_triplets_from_metadata.sh
@@ -5,19 +5,8 @@
 # echo "Created directory: outputs/kg/$CURR_TIME"
 
 # PATH_WORKSPACE=outputs/kg/$CURR_TIME
-# PATH_WORKSPACE=outputs/kg/20240222190741
-PATH_WORKSPACE=outputs/kg/merge_dbs/fa_v1
 
-# PATH_DATA_LIT2KG=data/Lit2KG/tests/text_parser_predicted.pkl
-# python -m food_atlas.kg.run_data_preparation \
-#     $PATH_DATA_LIT2KG \
-#     $PATH_WORKSPACE
+PATH_OUTPUT_DIR=outputs/kg/20240315
 
-python -m food_atlas.kg.run_entity_linkage \
-    $PATH_WORKSPACE
-
-# python -m food_atlas.kg.retrieve_ncbi_taxonomy \
-#     $PATH_WORKSPACE
-
-# python -m food_atlas.kg.retrieve_pubchem_compound \
-#     $PATH_WORKSPACE
+python -m food_atlas.kg.run_kg_expansion \
+    $PATH_OUTPUT_DIR
