5e759f9f1d6c2731db2c273405457335ed272f66
fangzhouli <fzli0805@gmail.com>
2024-03-29 21:30:13 -0700

    refactored to reuse kg code

--- food_atlas/kg/initialization/retrieve_dbs/retrieve_ncbi_taxonomy.py
+++ food_atlas/kg/initialization/retrieve_dbs/retrieve_ncbi_taxonomy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from Bio import Entrez
 
-with open("food_atlas/kg/initialization/retrieve_dbs/api_key.txt") as f:
+with open("food_atlas/kg/api_key.txt") as f:
     Entrez.email = f.readline().strip()
     Entrez.api_key = f.readline().strip()
 
