4de21a09c60527e2ce3a962c9aefe9100df72421
fangzhouli <fzli0805@gmail.com>
2024-03-29 21:30:48 -0700

    beautified chemical name lut

--- food_atlas/kg/utils/constants.py
+++ food_atlas/kg/utils/constants.py
@@ -1,15 +1,15 @@
 def get_lookup_key_by_id(id_type: str, id_value: str | int) -> str:
-    LOOKUP_BY_ID = "_FAEXTID:{}_SEP_{}"
+    LOOKUP_BY_ID = "{}:{}"
 
     if id_type == 'ncbi_taxon_id':
-        return LOOKUP_BY_ID.format('ncbi_taxon_id', id_value)
+        return LOOKUP_BY_ID.format('NCBI_Taxonomy_ID', id_value)
     elif id_type == 'pubchem_cid':
-        return LOOKUP_BY_ID.format('pubchem_cid', id_value)
+        return LOOKUP_BY_ID.format('PubChem_Compound_ID', id_value)
     elif id_type == 'fdc_nutrient_ids':
-        return LOOKUP_BY_ID.format('fdc_nutrient_ids', id_value)
+        return LOOKUP_BY_ID.format('FDC_Nutrient_ID', id_value)
     elif id_type == 'foodb_ids':
-        return LOOKUP_BY_ID.format('foodb_ids', id_value)
+        return LOOKUP_BY_ID.format('FooDB_Food_ID', id_value)
     elif id_type == 'fdc_ids':
-        return LOOKUP_BY_ID.format('fdc_ids', id_value)
+        return LOOKUP_BY_ID.format('FDC_ID', id_value)
     else:
         raise ValueError(f"Unknown ID type: {id_type}")
