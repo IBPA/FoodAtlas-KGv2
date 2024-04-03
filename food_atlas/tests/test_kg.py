4f2ff148713504fbf74584d4a0de6c1f48f28526
fangzhouli <fzli0805@gmail.com>
2024-04-03 13:12:37 -0700

    added unit tester

--- food_atlas/tests/test_kg.py
+++ food_atlas/tests/test_kg.py
@@ -1,36 +1,8 @@
-from pandarallel import pandarallel
-
+from .unit_test_kg import test_all
 from ..kg import KnowledgeGraph
-
-pandarallel.initialize(progress_bar=True)
 
 
 if __name__ == '__main__':
-    kg = KnowledgeGraph()
-
-    # Test 1. All synonyms in the entities can be found in LUT.
-    def _check_entity_name_in_lut(row):
-        for name in row['synonyms']:
-            if row['entity_type'] == 'food':
-                assert name in kg.entities._lut_food, \
-                    f"Test 1 failed: food {name} {row['foodatlas_id']}"
-            elif row['entity_type'] == 'chemical':
-                assert name in kg.entities._lut_chemical, \
-                    f"Test 1 failed: chemical {name} {row['foodatlas_id']}"
-            else:
-                raise NotImplementedError()
-
-    kg.entities._entities.parallel_apply(_check_entity_name_in_lut, axis=1)
-
-    # Test 2. All entities in the lookup table are in the entities.
-    entities_in_lut = set()
-    for entities in kg.entities._lut_food.values():
-        entities_in_lut.update(entities)
-    for entities in kg.entities._lut_chemical.values():
-        entities_in_lut.update(entities)
-    entities = set(kg.entities._entities.index.tolist())
-    assert len(entities) == len(kg.entities._entities)
-    assert entities_in_lut - entities == set(), "Test 2 failed."
-
-    # Test 3. All triplets have valid entities.
-    assert kg.triplets._triplets['foodatlas_id'].nunique() == len(kg.triplets._triplets)
+    kg = KnowledgeGraph(path_kg="outputs/kg/20240329")
+    test_all(kg)
+    print('All tests passed.')
--- food_atlas/tests/unit_test_kg.py
+++ food_atlas/tests/unit_test_kg.py
@@ -1,19 +1,4 @@
-def test_triplets(kg):
-    triplets = kg.triplets._triplets
-
-    # Test 1. Triplets should be unique.
-    assert triplets['foodatlas_id'].nunique() == len(triplets)
-    assert len(triplets.groupby(['head_id', 'relationship_id', 'tail_id']).size()) \
-        == len(triplets)
-
-    # Test 2. Metadata ids should be unique.
-    for _, row in triplets.iterrows():
-        assert len(row['metadata_ids']) == len(set(row['metadata_ids']))
-
-
-def test_metadata(kg):
-    # Test 1: For each master unit, let's provide a unit test case.
-    pass
+from ..kg import KnowledgeGraph
 
 
 def test_entities(kg):
@@ -31,8 +16,47 @@ def test_entities(kg):
         except AssertionError:
             print(k, v)
 
+    # Test 2: Each entity should neither be a "placeholding" or a "placeholden".
+    entities = kg.entities._entities
+    for _, row in entities.iterrows():
+        assert (
+            '_placeholder_to' in row['external_ids']
+            and '_placeholder_from' in row['external_ids']
+        ) is False
+
+
+def test_triplets(kg):
+    triplets = kg.triplets._triplets
+
+    # Test 1. Triplets should be unique.
+    assert triplets['foodatlas_id'].nunique() == len(triplets)
+    assert len(triplets.groupby(['head_id', 'relationship_id', 'tail_id']).size()) \
+        == len(triplets)
+
+    # Test 2. Metadata ids should be unique.
+    for _, row in triplets.iterrows():
+        assert len(row['metadata_ids']) == len(set(row['metadata_ids']))
+
+    # Test 3. A metadata id should not be shared across triplets.
+    metadata_ids = set()
+    for _, row in triplets.iterrows():
+        for metadata_id in row['metadata_ids']:
+            assert metadata_id not in metadata_ids
+            metadata_ids.add(metadata_id)
+
+
+def test_metadata(kg):
+    # Test 1: For each master unit, let's provide a unit test case.
+    pass
+
 
 def test_all(kg):
     test_entities(kg)
-    # test_triplets(kg)
+    test_triplets(kg)
     # test_metadata(kg)
+
+
+if __name__ == '__main__':
+    kg = KnowledgeGraph("outputs/kg/20240401")
+    test_all(kg)
+    print('All tests passed.')
