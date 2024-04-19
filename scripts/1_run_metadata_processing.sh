e54599735af9edb02a0bab2dc173a3da763c3ed3
fangzhouli <fzli0805@gmail.com>
2024-04-19 00:15:28 -0700

    added script

--- scripts/1_run_metadata_processing.sh
+++ scripts/1_run_metadata_processing.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# CURR_TIME=$(date "+%Y%m%d%H%M%S")
-# mkdir -p outputs/kg/$CURR_TIME
-# echo "Created directory: outputs/kg/$CURR_TIME"
-
-# PATH_WORKSPACE=outputs/kg/$CURR_TIME
-# PATH_WORKSPACE=outputs/kg/20240222190741
-
 PATH_INPUT=data/Lit2KG/text_parser_predicted_2024_02_25.pkl
-PATH_OUTPUT_DIR=outputs/kg/20240401
+PATH_OUTPUT_DIR=outputs/kg/20240418
 
 python -m food_atlas.kg.run_metadata_processing \
     $PATH_INPUT \
