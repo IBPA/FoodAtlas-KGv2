007f0fcbe5b6c3fe217a8542f8c0d48599335cc3
fangzhouli <fzli0805@gmail.com>
2024-04-19 01:54:54 -0700

    added logger and refactored

--- food_atlas/kg/__init__.py
+++ food_atlas/kg/__init__.py
@@ -1,4 +1,12 @@
+import logging
+
 from ._kg import KnowledgeGraph
+
+logging.basicConfig(
+    format='%(name)s %(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 __all__ = [
     'KnowledgeGraph',
