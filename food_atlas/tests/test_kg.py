da1309922e5a7742cde073361feac1122a24b539
fangzhouli <fzli0805@gmail.com>
2024-04-19 01:56:03 -0700

    added logger

--- food_atlas/tests/test_kg.py
+++ food_atlas/tests/test_kg.py
@@ -1,7 +1,11 @@
+import logging
+
 import click
 
 from .unit_test_kg import test_all
 from ..kg import KnowledgeGraph
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -10,9 +14,12 @@ from ..kg import KnowledgeGraph
     type=str,
 )
 def main(path_kg: str):
+    logger.info('Running unit tests for the knowledge graph...')
+
     kg = KnowledgeGraph(path_kg=path_kg)
     test_all(kg)
-    print('All tests passed.')
+
+    logger.info('All tests passed.')
 
 if __name__ == '__main__':
     main()
