fab540aa5aa942c0c867bdb15adaac5d2b5a8411
fangzhouli <fzli0805@gmail.com>
2024-04-30 10:35:31 -0700

    styled

--- food_atlas/kg/utils/constants.py
+++ food_atlas/kg/utils/constants.py
@@ -14,16 +14,3 @@ def get_lookup_key_by_id(id_type: str, id_value: str | int) -> str:
         raise ValueError(f"Unknown ID type: {id_type}")
 
     return LOOKUP_BY_ID.format(ID_PREFIX_MAPPER[id_type], id_value)
-
-    # if id_type == 'ncbi_taxon_id':
-    #     return LOOKUP_BY_ID.format('NCBI_Taxonomy_ID', id_value)
-    # elif id_type == 'pubchem_cid':
-    #     return LOOKUP_BY_ID.format('PubChem_Compound_ID', id_value)
-    # elif id_type == 'fdc_nutrient_ids':
-    #     return LOOKUP_BY_ID.format('FDC_Nutrient_ID', id_value)
-    # elif id_type == 'foodb_ids':
-    #     return LOOKUP_BY_ID.format('FooDB_Food_ID', id_value)
-    # elif id_type == 'fdc_ids':
-    #     return LOOKUP_BY_ID.format('FDC_ID', id_value)
-    # else:
-    #     raise ValueError(f"Unknown ID type: {id_type}")
