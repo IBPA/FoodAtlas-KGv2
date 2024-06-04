811865c937e486125da316f20071879871a10968
asmyoo <asmyoo@arielle>
2024-06-04 11:38:17 -0700

    Updating some minor scripts

--- data/download.sh
+++ data/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # # Download the data from USDA FDC.
-# https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_foundation_food_csv_2023-10-26.zip
+# https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_foundation_food_csv_2024-04-18.zip
 # https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_sr_legacy_food_csv_2018-04.zip
 
 # # Download the data from FoodAtlas zip.
