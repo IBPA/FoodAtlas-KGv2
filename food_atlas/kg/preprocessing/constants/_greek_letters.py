46af1c806b378852f01533d0b907f4756ef81dfe
fangzhouli <fzli0805@gmail.com>
2024-06-13 01:28:48 -0700

    improved chemical name standardization

---
+++ food_atlas/kg/preprocessing/constants/_greek_letters.py
@@ -0,0 +1,52 @@
+# Reference: https://en.wikipedia.org/wiki/Greek_script_in_Unicode
+ALPHA = 'α𝛂𝛼𝜶𝝰𝞪Α𝚨𝛢𝜜𝝖𝞐ἀἈἄἌᾄᾌἂἊᾂᾊἆἎᾆᾎᾀᾈἁἉἅἍᾅᾍἃἋᾃᾋἇἏᾇᾏᾁᾉάάΆΆᾴὰᾺᾲᾰᾸᾶᾷᾱᾹᾳᾼ' + '⍺ɑ'
+BETA = 'βϐ𝛃𝛽𝜷𝝱𝞫Β𝚩𝛣𝜝𝝗𝞑ᵝᵦ' + 'ßꞵ'
+GAMMA = 'γℽ𝛄𝛾𝜸𝝲𝞬Γℾ𝚪𝛤𝜞𝝘𝞒ᵞᵧᴦ' + 'ɣ'
+DELTA = 'δ𝛅𝛿𝜹𝝳𝞭Δ𝚫𝛥𝜟𝝙𝞓ᵟ' + '∆'
+EPSILON = 'εϵ𝛆𝛜𝜀𝜖𝜺𝝐𝝴𝞊𝞮𝟄Ε𝚬𝛦𝜠𝝚𝞔ἐἘἔἜἒἚἑἙἕἝἓἛέέΈΈὲῈ' + 'ɛ'
+ZETA = 'ζ𝛇𝜁𝜻𝝵𝞯Ζ𝚭𝛧𝜡𝝛𝞕'
+ETA = 'η𝛈𝜂𝜼𝝶𝞰Η𝚮𝛨𝜢𝝜𝞖ἠἨἤἬᾔᾜἢἪᾒᾚἦἮᾖᾞᾐᾘἡἩἥἭᾕᾝἣἫᾓᾛἧἯᾗᾟᾑᾙήήΉΉῄὴῊῂῆῇῃῌ'
+THETA = 'θϑ𝛉𝛝𝜃𝜗𝜽𝝑𝝷𝞋𝞱𝟅Θϴ𝚯𝚹𝛩𝛳𝜣𝜭𝝝𝝧𝞗𝞡ᶿ'
+OMEGA = 'ω𝛚𝜔𝝎𝞈𝟂ΩΩꭥ𝛀𝛺𝜴𝝮𝞨ὠὨὤὬᾤᾬὢὪᾢᾪὦὮᾦᾮᾠᾨὡὩὥὭᾥᾭὣὫᾣᾫὧὯᾧᾯᾡᾩώώΏΏῴὼῺῲῶῷῳῼꭥ'
+KAPPA = 'κ'
+LAMBDA = 'λ'
+IOTA = 'ι'
+SIGMA = '∑σ'
+TAO = 'τ'
+MU = 'μ'
+NU = 'ν'
+XI = 'ξ'
+OMICRON = 'ο'
+PI = 'π'
+RHO = 'ρ'
+UPSILON = 'υ'
+PHI = 'φ'
+CHI = 'χ'
+PSI = 'ψ'
+
+GREEK_LETTERS = {
+    'alpha': ALPHA,
+    'beta': BETA,
+    'gamma': GAMMA,
+    'delta': DELTA,
+    'epsilon': EPSILON,
+    'zeta': ZETA,
+    'eta': ETA,
+    'theta': THETA,
+    'omega': OMEGA,
+    'kappa': KAPPA,
+    'lambda': LAMBDA,
+    'iota': IOTA,
+    'sigma': SIGMA,
+    'tao': TAO,
+    'mu': MU,
+    'nu': NU,
+    'xi': XI,
+    'omicron': OMICRON,
+    'pi': PI,
+    'rho': RHO,
+    'upsilon': UPSILON,
+    'phi': PHI,
+    'chi': CHI,
+    'psi': PSI,
+}
---
+++ food_atlas/kg/preprocessing/constants/_punctuations.py
@@ -0,0 +1,14 @@
+# References: https://www.compart.com/en/unicode/category/Pd
+EMPTYS = '­  '
+HYPHENS = '‐−–‑—‒'
+DOUBLE_QUOTES = '“”″'
+SINGLE_QUOTES = '’′ʹˋ‘'
+ARROWS = '→'
+
+PUNCTUATIONS = {
+    '': EMPTYS,
+    '-': HYPHENS,
+    '"': DOUBLE_QUOTES,
+    '\'': SINGLE_QUOTES,
+    '->': ARROWS,
+}
