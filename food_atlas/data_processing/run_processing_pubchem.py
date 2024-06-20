3f43a3d655ea18f4048a38da02586529342dfa8a
fangzhouli <fzli0805@gmail.com>
2024-06-19 17:01:27 -0700

    added processing script for pubchem

---
+++ food_atlas/data_processing/run_processing_pubchem.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+    # The SID-Map file is too large to load frequentlt. We will ignore everything that
+    # is not from ChEBI and convert it to parquet.
+    sids = pd.read_csv(
+        "data/PubChem/SID-Map",
+        sep="\t",
+        header=None,
+        names=['SID', 'source', 'registry_id', 'cid'],
+    )
+
+    sids.query("source == 'ChEBI'").to_parquet(
+        "outputs/data_processing/pubchem-sid-map-small.parquet"
+    )
