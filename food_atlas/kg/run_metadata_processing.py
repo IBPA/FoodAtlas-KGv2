39cddb481b62e9e251fa33ab37825ad64b5af2d9
fangzhouli <fzli0805@gmail.com>
2024-06-26 21:41:08 -0700

    updated to reflect changes

--- food_atlas/kg/run_metadata_processing.py
+++ food_atlas/kg/run_metadata_processing.py
@@ -56,6 +56,7 @@ def format_tuple(
         'text': row['sentence'],
         'pmcid': row['pmcid'],
     }
+    row['entity_linking_method'] = 'exact_name_matching'
 
     return row
 
@@ -92,12 +93,18 @@ def main(
     print(f"# tuples extracted: {n_tuples}")
 
     columns = [
-        'conc_value', 'conc_unit',
-        'food_part', 'food_processing',
+        'conc_value',
+        'conc_unit',
+        'food_part',
+        'food_processing',
+        'source',
+        'reference',
+        'entity_linking_method',
         'quality_score',
-        'source', 'reference',
-        '_food_name', '_chemical_name',
-        '_conc', '_food_part',
+        '_food_name',
+        '_chemical_name',
+        '_conc',
+        '_food_part',
     ]
     metadata = data_raw.explode('tuples').dropna(subset=['tuples'])
     metadata[columns] = None
@@ -109,55 +116,56 @@ def main(
 
     # TODO: Improve.
     # For now, just assign FoodOn IDs by exact matching for foods.
-    lut_food = pd.read_csv(
-        "outputs/kg/lookup_table_food.tsv",
-        sep='\t',
-    )
+    lut_food = pd.read_csv("outputs/kg/lookup_table_food.tsv", sep='\t')
     lut_food = dict(zip(lut_food['name'], lut_food['foodatlas_id']))
-
+    lut_chemical = pd.read_csv("outputs/kg/lookup_table_chemical.tsv", sep='\t')
+    lut_chemical = dict(zip(lut_chemical['name'], lut_chemical['foodatlas_id']))
     metadata = metadata[metadata['_food_name'].apply(lambda x: x in lut_food)]
+    metadata = metadata[metadata['_chemical_name'].apply(lambda x: x in lut_chemical)]
+
     metadata.to_csv(
         f"{path_output_dir}/_metadata_new.tsv",
         sep='\t',
         index=False,
     )
 
-    # Get unique list of chemical mentions for the exchange service.
-    print("Generating chemical mentions...")
-    chemicals = metadata['_chemical_name'].unique()
-    chemicals = pd.DataFrame(chemicals)
-    chemicals.to_csv(
-        f"{path_output_dir}/_chemical_mentions.tsv",
-        sep='\t',
-        index=False,
-        header=False,
-    )
-    print(
-        f"Chemical mentions generated at `{path_output_dir}/_chemical_mentions.tsv`. "
-        "Use PubChem Identifier Exchange Service to get PubChem IDs and rename the "
-        f"file to `{path_output_dir}/mention_to_cid.txt`. Press Enter to continue."
-    )
-    input()
+    # exit()
+    # # Get unique list of chemical mentions for the exchange service.
+    # print("Generating chemical mentions...")
+    # chemicals = metadata['_chemical_name'].unique()
+    # chemicals = pd.DataFrame(chemicals)
+    # chemicals.to_csv(
+    #     f"{path_output_dir}/_chemical_mentions.tsv",
+    #     sep='\t',
+    #     index=False,
+    #     header=False,
+    # )
+    # print(
+    #     f"Chemical mentions generated at `{path_output_dir}/_chemical_mentions.tsv`. "
+    #     "Use PubChem Identifier Exchange Service to get PubChem IDs and rename the "
+    #     f"file to `{path_output_dir}/mention_to_cid.txt`. Press Enter to continue."
+    # )
+    # input()
 
-    # Get unique list of PubChem CIDs for the exchange service.
-    print("Generating PubChem CIDs...")
-    cids = pd.read_csv(
-        f"{path_output_dir}/mention_to_cid.txt",
-        sep='\t',
-        header=None,
-    )[1].astype({1: 'Int64'}).dropna().unique()
-    cids = pd.DataFrame(cids)
-    cids.to_csv(
-        f"{path_output_dir}/_cids.tsv",
-        sep='\t',
-        index=False,
-        header=False,
-    )
-    print(
-        f"PubChem CIDs generated at `{path_output_dir}/_cids.tsv`. "
-        "Use PubChem Identifier Exchange Service to get FoodOn IDs and rename the "
-        f"file to `{path_output_dir}/cid_to_chebi.txt`."
-    )
+    # # Get unique list of PubChem CIDs for the exchange service.
+    # print("Generating PubChem CIDs...")
+    # cids = pd.read_csv(
+    #     f"{path_output_dir}/mention_to_cid.txt",
+    #     sep='\t',
+    #     header=None,
+    # )[1].astype({1: 'Int64'}).dropna().unique()
+    # cids = pd.DataFrame(cids)
+    # cids.to_csv(
+    #     f"{path_output_dir}/_cids.tsv",
+    #     sep='\t',
+    #     index=False,
+    #     header=False,
+    # )
+    # print(
+    #     f"PubChem CIDs generated at `{path_output_dir}/_cids.tsv`. "
+    #     "Use PubChem Identifier Exchange Service to get FoodOn IDs and rename the "
+    #     f"file to `{path_output_dir}/cid_to_chebi.txt`."
+    # )
 
 
 if __name__ == '__main__':
