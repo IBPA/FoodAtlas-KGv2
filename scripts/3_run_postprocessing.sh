1622fa9da5bd754d76269573e1b509d43ce9ba5d
fangzhouli <fzli0805@gmail.com>
2024-07-12 18:26:56 -0700

    minor: removed the deprecated commands

--- scripts/3_run_postprocessing.sh
+++ scripts/3_run_postprocessing.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-python -m food_atlas.kg.postprocessing.map_chebi_ids
-
+# python -m food_atlas.kg.postprocessing.map_chebi_ids  # Deprecated
+# python -m food_atlas.kg.postprocessing.update_entity_common_name  # Deprecated
 python -m food_atlas.kg.postprocessing.generate_entity_group
-
-python -m food_atlas.kg.postprocessing.update_entity_common_name
-
 python -m food_atlas.kg.postprocessing.convert_unit
