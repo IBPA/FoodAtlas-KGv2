03a14eba30d948b3adc8799de1d0623748d069c9
fangzhouli <fzli0805@gmail.com>
2025-10-10 14:12:38 -0700

    adding prints for metadata processing

--- food_atlas/additional_analysis/print_foodatlas_metadata_stats.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-if __name__ == "__main__":
-    metadata_new = pd.read_csv("outputs/kg/2025_10_10/_metadata_new.tsv", sep='\t')
-    metadata_added = pd.read_csv("outputs/kg/metadata_contains.tsv", sep='\t')
-
-    print(metadata_new)
-    print(metadata_added)
--- food_atlas/kg/run_metadata_processing.py
+++ food_atlas/kg/run_metadata_processing.py
@@ -27,9 +27,20 @@ def standardize_food_part():
 
 def parse_triplets_from_response(
     row: pd.Series,
+    model_name: str,
 ):
-    triplets_str = row['response'].split('\n')
+    response = row['response']
+    if pd.isna(response):
+        return
+
+    triplets_str = response.split('\n')
     for triplet_str in triplets_str:
+        if model_name == 'gpt-3.5-ft':
+            if not triplet_str:
+                continue
+            if triplet_str[0] == '(' and triplet_str[-1] == ')':
+                triplet_str = triplet_str[1:-1]
+
         triplet = next(
             csv.reader(
                 [triplet_str],
@@ -38,6 +49,7 @@ def parse_triplets_from_response(
                 skipinitialspace=True,
             )
         )
+
         if len(triplet) == 4 and triplet[0] and triplet[2]:
             row['tuples'] += [triplet]
         else:
@@ -46,12 +58,13 @@ def parse_triplets_from_response(
 
 
 def format_tuple(
-    row: pd.Series
+    row: pd.Series,
+    model_name: str,
 ):
     row[['_food_name', '_food_part', '_chemical_name', '_conc']] \
         = [x.strip().lower() for x in row['tuples']]
     row['_chemical_name'] = standardize_chemical_name(row['_chemical_name'])
-    row['source'] = 'lit2kg:gpt-4'
+    row['source'] = f"lit2kg:{model_name}"
     row['reference'] = {
         'text': row['sentence'],
         'pmcid': row['pmcid'],
@@ -67,16 +80,28 @@ def format_tuple(
 @click.argument(
     'path-output-dir',
 )
+@click.option(
+    '--model-name',
+    default='gpt-4',
+    help='Model name used for parsing triplets.',
+)
 def main(
     path_input: str,
     path_output_dir: str,
+    model_name: str,
 ):
-    data_raw = pd.read_pickle(path_input)
+    if model_name not in ['gpt-4', 'gpt-3.5-ft']:
+        raise ValueError(f"Invalid model name: {model_name}")
+
+    if model_name == 'gpt-4':
+        data_raw = pd.read_pickle(path_input)
+    elif model_name == 'gpt-3.5-ft':
+        data_raw = pd.read_csv(path_input, sep='\t')
     print(f"# sentences: {len(data_raw)}")
 
     data_raw['tuples'] = [[] for _ in data_raw.index]
     data_raw['tuples_not_parsed'] = [[] for _ in data_raw.index]
-    data_raw.apply(parse_triplets_from_response, axis=1)
+    data_raw.apply(lambda row: parse_triplets_from_response(row, model_name), axis=1)
     data_raw_with_not_parsed = data_raw[data_raw['tuples_not_parsed'].apply(
         lambda x: True if len(x) != 0 else False
     )]
@@ -108,20 +133,43 @@ def main(
     ]
     metadata = data_raw.explode('tuples').dropna(subset=['tuples'])
     metadata[columns] = None
-    metadata = metadata.parallel_apply(format_tuple, axis=1)
+    metadata = metadata.parallel_apply(
+        lambda row: format_tuple(row, model_name),
+        axis=1,
+    )
     metadata = metadata[columns]
 
     # Stndardize the chemical concentration.
     metadata = standardize_chemical_conc(metadata)
 
-    # TODO: Improve.
-    # For now, just assign FoodOn IDs by exact matching for foods.
+    # Exact matching for foods and chemicals.
     lut_food = pd.read_csv("outputs/kg/lookup_table_food.tsv", sep='\t')
     lut_food = dict(zip(lut_food['name'], lut_food['foodatlas_id']))
     lut_chemical = pd.read_csv("outputs/kg/lookup_table_chemical.tsv", sep='\t')
     lut_chemical = dict(zip(lut_chemical['name'], lut_chemical['foodatlas_id']))
-    metadata = metadata[metadata['_food_name'].apply(lambda x: x in lut_food)]
-    metadata = metadata[metadata['_chemical_name'].apply(lambda x: x in lut_chemical)]
+
+    food_name_hits = metadata['_food_name'].apply(lambda x: x in lut_food)
+    chemical_name_hits = metadata['_chemical_name'].apply(lambda x: x in lut_chemical)
+
+    food_name_hit_rate = food_name_hits.sum() / len(metadata)
+    chemical_name_hit_rate = chemical_name_hits.sum() / len(metadata)
+    print(f"# food name hits: {food_name_hits.sum()} ({food_name_hit_rate:.4f})")
+    print(f"# chemical name hits: {chemical_name_hits.sum()} ({chemical_name_hit_rate:.4f})")
+
+    unique_food_names = metadata['_food_name'].unique()
+    unique_chemical_names = metadata['_chemical_name'].unique()
+    unique_food_name_hits = [x in lut_food for x in unique_food_names]
+    unique_chemical_name_hits = [x in lut_chemical for x in unique_chemical_names]
+    unique_food_name_hit_rate = sum(unique_food_name_hits) / len(unique_food_names)
+    unique_chemical_name_hit_rate = sum(unique_chemical_name_hits) / len(unique_chemical_names)
+    print(f"# unique food names: {len(unique_food_names)}")
+    print(f"# unique chemical names: {len(unique_chemical_names)}")
+    print(f"# unique food name hits: {sum(unique_food_name_hits)} ({unique_food_name_hit_rate:.4f})")
+    print(f"# unique chemical name hits: {sum(unique_chemical_name_hits)} ({unique_chemical_name_hit_rate:.4f})")
+
+    metadata = metadata[food_name_hits & chemical_name_hits]
+
+    print(f"# metadata after filtering: {len(metadata)}")
 
     metadata.to_csv(
         f"{path_output_dir}/_metadata_new.tsv",
