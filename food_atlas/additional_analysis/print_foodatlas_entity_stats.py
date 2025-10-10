0bc99938e0f7f12b7407822cf16f2a3d54e1cdce
fangzhouli <fzli0805@gmail.com>
2025-10-10 11:09:15 -0700

    updated flavor merging code for fuzzy matching

---
+++ food_atlas/additional_analysis/print_foodatlas_entity_stats.py
@@ -0,0 +1,184 @@
+from ast import literal_eval
+
+import pandas as pd
+
+
+def load_entities(with_connections=True):
+    """Load entities.tsv.
+
+    Args:
+        with_connections (bool): If True, load entities with at least one connection to
+            a food entity.
+
+    """
+    entities = pd.read_csv(
+        "outputs/kg/entities.tsv",
+        sep='\t',
+        converters={
+            'external_ids': literal_eval,
+        },
+    ).set_index('foodatlas_id')
+
+    if with_connections:
+        entities['count'] = 0
+        triplets = pd.read_csv(
+            "outputs/kg/triplets.tsv",
+            sep='\t',
+            converters={
+                'metadata_ids': literal_eval,
+            }
+        )
+        triplets_r1 = triplets[triplets['relationship_id'] == 'r1']
+        for _, row in triplets_r1.iterrows():
+            head_id = row['head_id']
+            tail_id = row['tail_id']
+            entities.at[head_id, 'count'] += len(row['metadata_ids'])
+            entities.at[tail_id, 'count'] += len(row['metadata_ids'])
+        entities = entities[entities['count'] > 0].copy()
+
+    return entities
+
+
+# def print_stats():
+#     triplets = pd.read_csv(
+#         "outputs/kg/triplets.tsv",
+#         sep='\t',
+#         converters={
+#             'metadata_ids': literal_eval,
+#         }
+#     )
+#     metadata_pos_disease = set()
+#     metadata_neg_disease = set()
+#     for _, row in triplets.query("relationship_id in ['r3', 'r4']").iterrows():
+#         if row['relationship_id'] == 'r3':
+#             metadata_pos_disease.update(row['metadata_ids'])
+#         else:
+#             metadata_neg_disease.update(row['metadata_ids'])
+
+#     print("Disease:")
+#     print(f"Metadata (Pos): {len(metadata_pos_disease)}")
+#     print(f"Metadata (Neg): {len(metadata_neg_disease)}")
+#     print()
+
+#     metadata_flavor = set()
+#     for _, row in triplets.query("relationship_id == 'r5'").iterrows():
+#         metadata_flavor.update(row['metadata_ids'])
+
+#     print("Flavor:")
+#     print(f"Metadata: {len(metadata_flavor)}")
+
+
+def print_stats_with_connections():
+    """Print various statistics for entities with connections.
+
+    """
+    entities = load_entities(True)
+    triplets = pd.read_csv(
+        "outputs/kg/triplets.tsv",
+        sep='\t',
+        converters={
+            'metadata_ids': literal_eval,
+        }
+    )
+    food_ontologies = pd.read_csv(
+        "outputs/kg/food_ontology.tsv",
+        sep='\t',
+    )
+    print(f"Ontologies: {len(food_ontologies)}")
+    chemical_ontologies = pd.read_csv(
+        "outputs/kg/chemical_ontology.tsv",
+        sep='\t',
+    )
+    print(f"Ontologies: {len(chemical_ontologies)}")
+    entities.query("entity_type == 'food'").sort_values(
+        'count',
+        ascending=False,
+    ).to_csv(
+        'foods_with_evidence_count.tsv',
+        sep='\t',
+    )
+
+    foods_with_connections = entities.query("entity_type == 'food' and count > 0")
+
+    print("Foods with connections:")
+    print(f"Entities: {len(foods_with_connections)}")
+    print()
+
+    chemicals = set(entities[entities['entity_type'] == 'chemical'].index.tolist())
+
+    print("Chemicals with connections:")
+    print(f"Entities: {len(chemicals)}")
+    print()
+
+    food_chemical_triplets = triplets.query("relationship_id == 'r1'")
+
+    print("Food-chemical associations:")
+    print(f"Triplets: {len(food_chemical_triplets)}")
+    print()
+
+    entities_disease = set()
+    triplets_pos_disease = set()
+    metadata_pos_disease = set()
+    triplets_neg_disease = set()
+    metadata_neg_disease = set()
+    for _, row in triplets.query("relationship_id in ['r3', 'r4']").iterrows():
+        if row['head_id'] not in chemicals:
+            continue
+        entities_disease.add(row['tail_id'])
+
+        if row['relationship_id'] == 'r3':
+            triplets_pos_disease.add(row['foodatlas_id'])
+            metadata_pos_disease.update(row['metadata_ids'])
+        else:
+            triplets_neg_disease.add(row['foodatlas_id'])
+            metadata_neg_disease.update(row['metadata_ids'])
+
+    print("Disease with connections:")
+    print(f"Entities: {len(entities_disease)}")
+    print(f"Triplets (Pos): {len(triplets_pos_disease)}")
+    print(f"Metadata (Pos): {len(metadata_pos_disease)}")
+    print(f"Triplets (Neg): {len(triplets_neg_disease)}")
+    print(f"Metadata (Neg): {len(metadata_neg_disease)}")
+    print()
+
+    entities_flavor = set()
+    triplets_flavor = set()
+    metadata_flavor = set()
+    for _, row in triplets.query("relationship_id == 'r5'").iterrows():
+        if row['head_id'] not in chemicals:
+            continue
+        entities_flavor.add(row['tail_id'])
+        triplets_flavor.add(row['foodatlas_id'])
+        metadata_flavor.update(row['metadata_ids'])
+
+    print("Flavor with connections:")
+    print(f"Entities: {len(entities_flavor)}")
+    print(f"Triplets: {len(triplets_flavor)}")
+    print(f"Metadata: {len(metadata_flavor)}")
+
+    # All triplets with connections
+    triplets_r1 = triplets[(triplets['relationship_id'] == 'r1') & (triplets['head_id'].isin(foods_with_connections.index))]
+
+    def dfs(onto, entities):
+        visited = set()
+        is_a_map = {row['head_id']: row['tail_id'] for _, row in onto.iterrows()}
+        for entity in entities:
+            curr = entity
+            while curr in is_a_map and curr not in visited:
+                visited.add(curr)
+                curr = is_a_map[curr]
+        return len(visited)
+    n_triplets_r2 = dfs(food_ontologies, foods_with_connections.index) + dfs(chemical_ontologies, list(chemicals))
+    triplets_r3 = triplets[(triplets['relationship_id'] == 'r3') & (triplets['head_id'].isin(chemicals))]
+    triplets_r4 = triplets[(triplets['relationship_id'] == 'r4') & (triplets['head_id'].isin(chemicals))]
+    triplets_r5 = triplets[(triplets['relationship_id'] == 'r5') & (triplets['head_id'].isin(chemicals))]
+    print(f"Triplets (r1): {len(triplets_r1)}")
+    print(f"Triplets (r2): {n_triplets_r2}")
+    print(f"Triplets (r3): {len(triplets_r3)}")
+    print(f"Triplets (r4): {len(triplets_r4)}")
+    print(f"Triplets (r5): {len(triplets_r5)}")
+
+    print(f"Total: {len(triplets_r1) + n_triplets_r2 + len(triplets_r3) + len(triplets_r4) + len(triplets_r5)}")
+
+if __name__ == '__main__':
+    print_stats_with_connections()
