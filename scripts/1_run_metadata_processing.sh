ac8bb40ee8321c0b0719acf49ae8ec486636c5d4
fangzhouli <fzli0805@gmail.com>
2025-10-13 14:57:46 -0700

    added scripts

--- scripts/1_run_metadata_processing.sh
+++ scripts/1_run_metadata_processing.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PATH_INPUT=data/Lit2KG/text_parser_predicted_2024_02_25.pkl
-PATH_OUTPUT_DIR=outputs/kg/20240626
+PATH_INPUT=data/Lit2KG/2024_12_31/text_parser_predicted_gpt3.tsv
+PATH_OUTPUT_DIR=outputs/kg/2025_10_10
 
 python -m food_atlas.kg.run_metadata_processing \
     $PATH_INPUT \
-    $PATH_OUTPUT_DIR
+    $PATH_OUTPUT_DIR \
+    --model-name gpt-3.5-ft
